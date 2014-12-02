24f95257c13defa47c65a88fed0b6de43b6f236d
Zandor300 <info@zsinfo.nl>
2014-12-02 22:01:47 +0100

    Some required classes and methods for mods.

--- src/main/java/com/zandor300/advancedfoods/AdvancedFoods.java
+++ src/main/java/com/zandor300/advancedfoods/AdvancedFoods.java
@@ -1,7 +1,34 @@
 package com.zandor300.advancedfoods;
 
+import com.zandor300.advancedfoods.proxy.ServerProxy;
+import com.zandor300.advancedfoods.reference.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+
 /**
  * Created by Zandor300 on 2-12-2014.
  */
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class AdvancedFoods {
+
+    @SidedProxy(clientSide = Reference.CLIENTSIDE, serverSide = Reference.SERVERSIDE)
+    public static ServerProxy Proxy;
+
+    @Mod.EventHandler
+    public static void PreInit(FMLPostInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public static void Init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public static void PostInit(FMLPostInitializationEvent event) {
+
+    }
 }
---
+++ src/main/java/com/zandor300/advancedfoods/proxy/ClientProxy.java
@@ -0,0 +1,13 @@
+package com.zandor300.advancedfoods.proxy;
+
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
+/**
+ * Created by Smith on 2-12-2014.
+ */
+public class ClientProxy extends ServerProxy {
+
+    public void registerRenderInfo() {
+
+    }
+}
