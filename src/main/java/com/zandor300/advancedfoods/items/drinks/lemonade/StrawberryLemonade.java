f17cd5ba059d2f0f047b8e995a33c10e8e9f56a9
Zandor300 <info@zsinfo.nl>
2014-12-03 09:37:37 +0100

    Preperation for Lemonade.

---
+++ src/main/java/com/zandor300/advancedfoods/items/drinks/lemonade/StrawberryLemonade.java
@@ -0,0 +1,15 @@
+package com.zandor300.advancedfoods.items.drinks.lemonade;
+
+import com.zandor300.advancedfoods.generic.AFItemFood;
+
+/**
+ * Created by Zandor on 3-12-2014.
+ */
+public class StrawberryLemonade extends AFItemFood {
+
+    public StrawberryLemonade(int id, int heal, boolean b) {
+        super(id, b);
+        this.setUnlocalizedName("StrawberryLemonade");
+    }
+
+}
