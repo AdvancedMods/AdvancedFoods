4e5ddf0cc6ded1192630cd742ac82b2d4043b71a
Zandor300 <info@zsinfo.nl>
2014-12-03 10:52:21 +0100

    Added recipes for Smooties.

--- src/main/java/com/zandor300/advancedfoods/initialization/Recipes.java
+++ src/main/java/com/zandor300/advancedfoods/initialization/Recipes.java
@@ -1,6 +1,9 @@
 package com.zandor300.advancedfoods.initialization;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
@@ -12,9 +15,14 @@ public class Recipes {
 
 	public static void init() {
 		// Drinks
+		// Lemonade
 		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.lemonade), new ItemStack(ModItems.smoothyGlass), new ItemStack(ModItems.lemon)));
 		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.cherryLemonade), new ItemStack(ModItems.lemonade), new ItemStack(ModItems.cherry)));
 		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.strawberryLemonade), new ItemStack(ModItems.lemonade), new ItemStack(ModItems.strawberry)));
+
+		// Smoothy
+		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.cherrySmoothy), new ItemStack(ModItems.smoothyGlass), new ItemStack(Blocks.ice), new ItemStack(ModItems.cherry)));
+		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.strawberrySmoothy), new ItemStack(ModItems.smoothyGlass), new ItemStack(Blocks.ice), new ItemStack(ModItems.strawberry)));
 
 		// Misc
 
