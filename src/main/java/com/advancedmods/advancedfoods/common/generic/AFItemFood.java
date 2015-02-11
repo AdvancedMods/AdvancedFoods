e4449889717b698df53cdd6a318176b05dc6e799
Dennisbonke <dennisb@kabelfoon.net>
2015-02-11 12:24:27 +0100

    Added more generic stuff.

---
+++ src/main/java/com/advancedmods/advancedfoods/common/generic/AFItemFood.java
@@ -0,0 +1,40 @@
+package com.advancedmods.advancedfoods.common.generic;
+
+import com.advancedmods.advancedfoods.core.AFProps;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Created by Dennisbonke on 11-2-2015.
+ */
+public class AFItemFood extends ItemFood {
+
+    public AFItemFood(int id, boolean b) {
+        super(id, b);
+        this.setCreativeTab(AFCreativeTab.food);
+    }
+
+    @Override
+    public String getUnlocalizedName() {
+        return String.format("item.%s%s", AFProps.modid.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack) {
+        return String.format("item.%s%s", AFProps.modid.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister) {
+        itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName) {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+
+}
