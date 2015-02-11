7d61da6684b2d3bf7fdab7f6ac05d08f64e352ea
Zandor300 <info@zsinfo.nl>
2015-02-11 13:12:55 +0100

    Added chilli, chocolate, chocolatecake, chocolatecupcake, cupcake, fishsandwich and hotdog.

---
+++ src/main/java/com/advancedmods/advancedfoods/common/items/food/ChocolateCupcake.java
@@ -0,0 +1,15 @@
+package com.advancedmods.advancedfoods.common.items.food;
+
+import com.advancedmods.advancedfoods.common.generic.AFItemFood;
+
+/**
+ * Created by Zandor on 11-2-2015.
+ */
+public class ChocolateCupcake extends AFItemFood {
+
+	public ChocolateCupcake(int id, int heal, boolean b) {
+		super(id, b);
+		this.setUnlocalizedName("ChocolateCupcake");
+	}
+
+}
