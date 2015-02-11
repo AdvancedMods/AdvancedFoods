3361a2ff3c0b81db2b750ca68f214b5c555f5fb9
Dennisbonke <dennisb@kabelfoon.net>
2015-02-11 11:17:29 +0100

    Added UpdateManager.

--- src/main/java/com/advancedmods/advancedfoods/AdvancedFoods.java
+++ src/main/java/com/advancedmods/advancedfoods/AdvancedFoods.java
@@ -2,9 +2,11 @@ package com.advancedmods.advancedfoods;
 
 import com.advancedmods.advancedfoods.common.CommonProxy;
 import com.advancedmods.advancedfoods.core.AFProps;
+import com.dennisbonke.dbcore.core.mod.BaseMod;
+import com.dennisbonke.dbcore.core.mod.updater.UpdateManager;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -16,28 +18,32 @@ import org.apache.logging.log4j.Logger;
  * Created by Dennisbonke on 10-2-2015.
  */
 @Mod(name = AFProps.name, modid = AFProps.modid, version = AFProps.version, dependencies = AFProps.dependencies, modLanguage = "java", canBeDeactivated = false, acceptedMinecraftVersions = AFProps.ACCEPTED_MC_VERSION)
-public class AdvancedFoods {
+public class AdvancedFoods extends BaseMod {
 
     @SidedProxy(clientSide = AFProps.clientproxy, serverSide = AFProps.commonproxy)
     public static CommonProxy proxy;
     @Instance(AFProps.modid)
     public static AdvancedFoods instance;
     public static Logger log = LogManager.getLogger("AdvancedFoods");
+    public static final String releaseURL = "https://raw.github.com/AdvancedMods/AdvancedFoods/master/VERSION";
 
     @EventHandler
-    public static void preInit(FMLPreInitializationEvent event) {
+    public void preInit(FMLPreInitializationEvent event) {
 
         // Starting mod
         log.info("Starting Advanced Foods version " + AFProps.version + "...");
         log.info("Entering Pre-Init phase...");
         // Do Pre-Init stuff
         proxy.preInit();
+        // Update manager
+        log.info("Starting Update manager for Advanced Foods...");
+        UpdateManager.registerUpdater(new UpdateManager(this, "https://raw.github.com/AdvancedMods/AdvancedFoods/master/VERSION", null));
         log.info("Pre-Init complete");
 
     }
 
     @EventHandler
-    public static void Init(FMLInitializationEvent event) {
+    public void Init(FMLInitializationEvent event) {
 
         log.info("Entering Init phase...");
         // Do Init stuff
@@ -47,7 +53,7 @@ public class AdvancedFoods {
     }
 
     @EventHandler
-    public static void postInit(FMLPostInitializationEvent event) {
+    public void postInit(FMLPostInitializationEvent event) {
 
         log.info("Entering Post-Init phase...");
         // Do Post-Init stuff
@@ -55,6 +61,21 @@ public class AdvancedFoods {
         log.info("Post-Init complete");
         log.info("Mod loaded");
 
+    }
+
+    @Override
+    public String getModId() {
+        return AFProps.modid;
+    }
+
+    @Override
+    public String getModName() {
+        return AFProps.name;
+    }
+
+    @Override
+    public String getModVersion() {
+        return AFProps.version;
     }
 
 }
