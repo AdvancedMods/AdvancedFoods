0e9f4a12fa408bdc8cf75b7aca468aaa2f6d6211
Dennisbonke <dennisb@kabelfoon.net>
2015-02-11 12:37:18 +0100

    ModBlocks.

--- src/main/java/com/advancedmods/advancedfoods/common/init/ModBlocks.java
+++ src/main/java/com/advancedmods/advancedfoods/common/init/ModBlocks.java
@@ -4,4 +4,23 @@ package com.advancedmods.advancedfoods.common.init;
  * Created by Dennisbonke on 11-2-2015.
  */
 public class ModBlocks {
+
+    //public static Block bananaPlant = new BananaPlant();
+    //public static Block cherryPlant = new CherryPlant();
+    //public static Block chilliPlant = new ChilliPlant();
+    //public static Block kiwiPlant = new KiwiPlant();
+    //public static Block lemonPlant = new LemonPlant();
+    //public static Block strawberryPlant = new StrawberryPlant();
+    //public static Block tomatoPlant = new TomatoPlant();
+
+    public static void init() {
+        //GameRegistry.registerBlock(bananaPlant, bananaPlant.getUnlocalizedName());
+        //GameRegistry.registerBlock(cherryPlant, cherryPlant.getUnlocalizedName());
+        //GameRegistry.registerBlock(chilliPlant, chilliPlant.getUnlocalizedName());
+        //GameRegistry.registerBlock(kiwiPlant, kiwiPlant.getUnlocalizedName());
+        //GameRegistry.registerBlock(lemonPlant, lemonPlant.getUnlocalizedName());
+        //GameRegistry.registerBlock(strawberryPlant, strawberryPlant.getUnlocalizedName());
+        //GameRegistry.registerBlock(tomatoPlant, tomatoPlant.getUnlocalizedName());
+    }
+
 }
