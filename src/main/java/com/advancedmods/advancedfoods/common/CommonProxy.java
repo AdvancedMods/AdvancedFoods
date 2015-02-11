f3eacf030f752a74ae7d0aca24eb5812b0385d22
Dennisbonke <dennisb@kabelfoon.net>
2015-02-11 12:55:23 +0100

    Updated CommonProxy.

--- src/main/java/com/advancedmods/advancedfoods/common/CommonProxy.java
+++ src/main/java/com/advancedmods/advancedfoods/common/CommonProxy.java
@@ -1,11 +1,24 @@
 package com.advancedmods.advancedfoods.common;
 
+import com.advancedmods.advancedfoods.AdvancedFoods;
+import com.advancedmods.advancedfoods.common.init.ModBlocks;
+import com.advancedmods.advancedfoods.common.init.ModItems;
+
 /**
  * Created by Dennisbonke on 11-2-2015.
  */
 public class CommonProxy {
 
     public static void preInit(){
+
+        // ModItems
+        AdvancedFoods.log.debug("Loading Items...");
+        ModItems.init();
+        AdvancedFoods.log.debug("Items loaded");
+        // ModBlocks
+        AdvancedFoods.log.debug("Loading Blocks...");
+        ModBlocks.init();
+        AdvancedFoods.log.debug("Blocks loaded");
 
     }
 
