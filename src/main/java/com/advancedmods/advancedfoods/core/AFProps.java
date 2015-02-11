99db4701b1b6d2d5cdf324b0e59b0b207d3b88c0
Dennisbonke <dennisb@kabelfoon.net>
2015-02-11 13:51:08 +0100

    Derp Derp Derp Derp Derp Derp Derp Derp Derp Derp Derp Derp .

--- src/main/java/com/advancedmods/advancedfoods/core/AFProps.java
+++ src/main/java/com/advancedmods/advancedfoods/core/AFProps.java
@@ -17,7 +17,7 @@ public class AFProps {
     public static final String name = "Advanced Foods";
     public static final String modid = "advancedfoods";
     public static final String version = REQUIRED_MC_VERSION + "-" + VERSION_BASE + "-" + VERSION_IDENTIFIER;
-    public static final String dependencies = "required-after:FML@[" + FORGE_BASE + FORGE_BUILD + "]" + ";" + "required-after:DBCore";
+    public static final String dependencies = "required-after:FML@[" + FORGE_BASE + FORGE_BUILD + "]" + ";" + "required-after:dbcore";
     public static final String commonproxy = "com.advancedmods.advancedfoods.common.CommonProxy";
     public static final String clientproxy = "com.advancedmods.advancedfoods.client.ClientProxy";
 
