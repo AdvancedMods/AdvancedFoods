e6405b4a17fc95e7284e0a6192212a3da9a57e72
Zandor300 <info@zsinfo.nl>
2015-02-12 12:55:22 +0100

    + Added wasabi.

--- src/main/java/com/advancedmods/advancedfoods/common/items/sauces/Sambal.java
+++
@@ -1,8 +0,0 @@
-package com.advancedmods.advancedfoods.common.items.sauces;
-
-/**
- * Created by Zandor on 12-2-2015.
- */
-public class Sambal {
-
-}
---
+++ src/main/java/com/advancedmods/advancedfoods/common/items/sauces/Wasabi.java
@@ -0,0 +1,16 @@
+package com.advancedmods.advancedfoods.common.items.sauces;
+
+import com.advancedmods.advancedfoods.common.generic.AFCreativeTab;
+import com.advancedmods.advancedfoods.common.generic.AFItem;
+
+/**
+ * Created by Zandor on 12-2-2015.
+ */
+public class Wasabi extends AFItem {
+
+	public Wasabi() {
+		super(AFCreativeTab.sauces);
+		setUnlocalizedName("wasabi");
+	}
+
+}
